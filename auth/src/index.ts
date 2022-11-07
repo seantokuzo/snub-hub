2458db7ab4959d6afda6ede44c2f979d021e7ad9
seantokuzo <seantokuzo@gmail.com>
2022-11-07 14:56:20 -0800

    auth index.ts changes for testing

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -1,9 +1,16 @@
-import express from "express"
-import { json } from "body-parser"
+import express from 'express'
+import { json } from 'body-parser'
 
 const app = express()
 app.use(json())
 
+app.get('/api/users/currentuser', (req, res) => {
+  console.log('Get current user')
+
+  res.send('Hello biiitch')
+})
+
 app.listen(3000, () => {
-  console.log("Listening on port 3000")
+  console.log('v20')
+  console.log('Listening on port 3000')
 })
