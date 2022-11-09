5f5bde9280ef0bae0e9d4e13b8e80d3ed7fb7227
seantokuzo <seantokuzo@gmail.com>
2022-11-08 21:08:11 -0800

    add initial route handlers and error handler middleware skeleton

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -1,16 +1,23 @@
 import express from 'express'
 import { json } from 'body-parser'
 
+import { currentUserRouter } from './routes/current-user'
+import { signinRouter } from './routes/signin'
+import { signoutRouter } from './routes/signout'
+import { signupRouter } from './routes/signup'
+import { errorHandler } from './middlewares/error-handler'
+
 const app = express()
 app.use(json())
 
-app.get('/api/users/currentuser', (req, res) => {
-  console.log('Get current user')
+app.use(currentUserRouter)
+app.use(signinRouter)
+app.use(signoutRouter)
+app.use(signupRouter)
 
-  res.send('Hello biiitch')
-})
+app.use(errorHandler)
 
 app.listen(3000, () => {
   console.log('v20')
-  console.log('Listening on port 3000')
+  console.log('🎧 Listening on port 3000')
 })
