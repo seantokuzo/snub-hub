5b15f0b630998c1924d392b02057634a68f53149
seantokuzo <seantokuzo@gmail.com>
2022-11-25 13:20:12 -0800

    finish basic tests for auth service

--- auth/src/test/setup.ts
+++ auth/src/test/setup.ts
@@ -1,6 +1,19 @@
 import { MongoMemoryServer } from 'mongodb-memory-server'
 import mongoose from 'mongoose'
+import request from 'supertest'
 import { app } from '../app'
+
+declare global {
+  var signin: () => Promise<string[]>
+}
+
+// declare global {
+//   namespace NodeJS {
+//     interface Global {
+//       signin(): Promise<string[]>
+//     }
+//   }
+// }
 
 let mongo: any
 
@@ -27,3 +40,17 @@ afterAll(async () => {
   }
   await mongoose.connection.close()
 })
+
+global.signin = async () => {
+  const email = 'test@test.com'
+  const password = 'test1234'
+
+  const response = await request(app)
+    .post('/api/users/signup')
+    .send({ email, password })
+    .expect(201)
+
+  const cookie = response.get('Set-Cookie')
+
+  return cookie
+}
